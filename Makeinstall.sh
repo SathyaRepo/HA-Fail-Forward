d863abc2e06747e2e71538cad1c256f7d4c70a9f
SathyaRepo <132055343+SathyaRepo@users.noreply.github.com>
2024-06-03 11:05:28 +0530

    Update Makeinstall.sh

--- Makeinstall.sh
+++ Makeinstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 docker volume create haproxy-etc
-docker run -d --name iPLON-HAProxy --network influxdb-net -v haproxy-etc:/usr/local/etc/haproxy/ -p 8088:8088 my-haproxy
+
+docker build -t iPLON-Load-Balancing-Service:1.0 .
+
+sleep 10;
+
+docker run -d --name iPLON-HAProxy --network influxdb-net -v haproxy-etc:/usr/local/etc/haproxy/ -p 8088:8088 itd iPLON-Load-Balancing-Service:1.0
